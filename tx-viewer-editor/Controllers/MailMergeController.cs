af901863c8e742a6b20cfb7add8dec20563d49d2
Bjoern Meyer <bjoern@textcontrol.com>
2024-11-08 15:28:46 +0100

    Add project files.

---
+++ tx-viewer-editor/Controllers/MailMergeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TXTextControl.DocumentServer;
+
+namespace tx_viewer_editor.Controllers
+{
+    public class MailMergeController : Controller
+    {
+        [HttpPost]
+        public string Merge([FromBody] string Template)
+        {
+            byte[] template = System.Convert.FromBase64String(Template);
+
+            using (TXTextControl.ServerTextControl tx = new TXTextControl.ServerTextControl())
+            {
+                tx.Create();
+                tx.Load(template, TXTextControl.BinaryStreamType.InternalUnicodeFormat);
+               
+                MailMerge mailMerge = new MailMerge();
+                mailMerge.TextComponent = tx;
+
+                string jsonData = System.IO.File.ReadAllText("data.json");
+
+                mailMerge.MergeJsonData(jsonData);
+
+                byte[] results;
+
+                tx.Save(out results, TXTextControl.BinaryStreamType.InternalUnicodeFormat);
+
+                return System.Convert.ToBase64String(results);
+            }
+        }
+    }
+}
---
+++ tx-viewer-editor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tx_viewer_editor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
